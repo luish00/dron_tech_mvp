e20ef3423fbb9c0d8ea172307f7ca59a905a9d84
Luis Hector <luishector.a2@gmail.com>
2020-07-15 01:39:37 -0500

    caroucel coments

--- js/init.js
+++ js/init.js
@@ -4,5 +4,28 @@
     $('.sidenav').sidenav();
     $('.parallax').parallax();
 
+    $('.carousel.carousel-slider').carousel({
+      fullWidth: true,
+      indicators: true,
+      noWrap: true,
+    });
+
+    // hotfix
+    document.getElementsByClassName("carousel")[0].removeAttribute('style');
+    const elem =  $('.carousel.carousel-slider');
+    const instance = M.Carousel.getInstance(elem);
+
+    $('#carousel-arrow-left').on('click', function() {
+      console.log('click')
+
+      instance.prev()
+    });
+
+    $('#carousel-arrow-right').on('click', function() {
+      console.log('click')
+
+      instance.next()
+    });
+
   }); // end of document ready
 })(jQuery); // end of jQuery name space
